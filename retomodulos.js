8abf8cb52a8e29c0b6d7a86e2dfa19b6ea04e947
jhenacerarellano <148493533+jhenacerarellano@users.noreply.github.com>
2023-12-01 12:00:35 -0600

    Add files via upload

---
+++ retomodulos.js
@@ -0,0 +1,13 @@
+// export function validarCorreo(correo){
+//     if (correo === "ejemplo@gmail.com"){
+//     console.log("el correo es valido");
+// } else {
+//     console.log("correo invalido")
+//    };
+// }
+// validarCorreo("ejemplo@gmail.com");
+console.log("--------------------------------")
+import {validarCorreo} from "./validarcorreo.js";
+const = new validarCorreo();
+console.log(validarCorreo);
+
